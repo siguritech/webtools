c50aadc994a5925461e3921e5033272ffe9d4104
siguritech <77058644+siguritech@users.noreply.github.com>
2022-10-22 16:52:09 +0200

    save to file

--- st-ip2asn.py
+++ st-ip2asn.py
@@ -1,12 +1,11 @@
-import sys
-from ipwhois.net import Net
-from ipwhois.asn import IPASN
-
 with open(sys.argv[1], 'r') as file:
     for line in file:
         if line:
             obj = IPASN(Net(line.strip()))
             result = obj.lookup()
-            txt = "[[{line}]] [[{asn}]] {result}".format(line=line.strip(), asn=result['asn'], result=result)
-            print(txt)
+            txt = "#ASN-{asn} \n\n {result}".format(asn=result['asn'], result=result)
+            fname = line.strip() + ".md"
+            f = open(fname, "w")
+            f.write(txt)
+            f.close()
             
