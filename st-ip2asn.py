8a4e71161c0e6c04949c77d2b798eef42a3bb3fb
siguritech <77058644+siguritech@users.noreply.github.com>
2022-10-22 16:33:41 +0200

    Update st-ip2asn.py

--- st-ip2asn.py
+++ st-ip2asn.py
@@ -1,11 +1,12 @@
 import sys
 from ipwhois.net import Net
 from ipwhois.asn import IPASN
-from pprint import pprint
 
 with open(sys.argv[1], 'r') as file:
     for line in file:
         if line:
             obj = IPASN(Net(line.strip()))
             result = obj.lookup()
-            pprint(result)
+            txt = "[[{line}]] [[{asn}]] {result}".format(line=line.strip(), asn=result['asn'], result=result)
+            print(txt)
+            
